d7eb406ccf35dc773f02d0c784b3b1c537fc15c4
ChinaskiJr <robin.colombier@gmail.com>
2019-06-07 09:21:29 +0200

    End DAL of id 2004

--- src/fr/eni/projet_encheres/dal/dao/DAOArticleVendu.java
+++ src/fr/eni/projet_encheres/dal/dao/DAOArticleVendu.java
@@ -1,7 +1,12 @@
 package fr.eni.projet_encheres.dal.dao;
 
 import fr.eni.projet_encheres.bo.ArticleVendu;
+import fr.eni.projet_encheres.bo.Categorie;
+import fr.eni.projet_encheres.dal.DALException;
+
+import java.util.List;
 
 public interface DAOArticleVendu extends DAO<ArticleVendu> {
-    
+    List<ArticleVendu> filterByCategory(Categorie categorie) throws DALException;
+    List<ArticleVendu> filterByString(String filter) throws DALException;
 }
